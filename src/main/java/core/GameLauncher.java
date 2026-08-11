6e225b2291b357f3a5913dcec72c54e84f52b6b8
Vince Emperado <vince_emperado@dlsu.edu.ph>
2026-08-11 12:05:28 +0800

    Started coding the GameLauncher class. This class extends JFrame, creating a window for the game.

--- src/main/java/core/GameLauncher.java
+++ src/main/java/core/GameLauncher.java
@@ -1,4 +1,25 @@
 package core;
 
-public class GameLauncher {
+import javax.swing.*;
+
+public class GameLauncher extends JFrame {
+    public static void main(String[] args) {
+        // Creates the window object, the title at the top bar will be Saga in Ephyra
+        JFrame gameFrame = new JFrame("Saga in Ephyra");
+
+        // Ensures that the window will close whenever the X button is pressed
+        gameFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        // Set the dimensions of the window, in this case it will be 800x600
+        gameFrame.setSize(800, 600);
+
+        // The will force the window to stay on the same size, the user will not be able to resize it
+        gameFrame.setResizable(false);
+
+        // Centers the window on the screen
+        gameFrame.setLocationRelativeTo(null);
+
+        // Makes the window visible
+        gameFrame.setVisible(true);
+    }
 }
