aca7baf4831f8ea8fc7339398a221c71fc87064b
matthewylanan-sirius <matthew_ylanan@dlsu.edu.ph>
2026-08-13 23:51:10 +0800

    Added Pattern Spawner

--- src/main/java/combat/PatternSpawner.java
+++ src/main/java/combat/PatternSpawner.java
@@ -1,4 +1,33 @@
 package combat;
 
+import pools.BulletPool;
+import java.awt.Color;
+import java.awt.image.BufferedImage;
+
 public class PatternSpawner {
+    private BulletPool bulletPool;
+
+    public PatternSpawner(BulletPool bulletPool) {
+        this.bulletPool = bulletPool;
+    }
+
+    /**
+     * Executes any FirePattern strategy implementation.
+     */
+    public void fire(FirePattern pattern, double originX, double originY, BufferedImage sprite, Color fallbackColor) {
+        if (bulletPool != null && pattern != null) {
+            pattern.execute(bulletPool, originX, originY, sprite, fallbackColor);
+        }
+    }
+
+    /**
+     * Convenience method to spawn a pincer pattern.
+     */
+    public void spawnPincer(double x, double y, double offset, int bulletsPerSide, double speed, double inwardAngleRad, BufferedImage sprite, Color color) {
+        fire(new PincerPattern(offset, bulletsPerSide, speed, inwardAngleRad), x, y, sprite, color);
+    }
+
+    // Getters & Setters
+    public BulletPool getBulletPool() { return bulletPool; }
+    public void setBulletPool(BulletPool bulletPool) { this.bulletPool = bulletPool; }
 }
