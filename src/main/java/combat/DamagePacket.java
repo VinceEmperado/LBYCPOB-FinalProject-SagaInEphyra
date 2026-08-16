75d22096d22ada40cea1745a107420c386bb44d1
trigger <triggerhappy1743@gmail.com>
2026-08-16 15:17:24 +0800

    added constructors and setters in damagepacket

--- src/main/java/combat/DamagePacket.java
+++ src/main/java/combat/DamagePacket.java
@@ -1,4 +1,21 @@
 package combat;
 
 public class DamagePacket {
+    private double amount;
+
+    public DamagePacket() {
+        this.amount = 0.0;
+    }
+
+    public DamagePacket(double amount) {
+        this.amount = amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
 }
