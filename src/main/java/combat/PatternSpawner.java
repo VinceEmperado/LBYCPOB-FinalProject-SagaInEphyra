86b4024e78b9d21321c143ef1dc7e67e6d36ed15
trigger <triggerhappy1743@gmail.com>
2026-08-16 15:13:37 +0800

    updated patternspawner for claw and pincer patterns

--- src/main/java/combat/PatternSpawner.java
+++ src/main/java/combat/PatternSpawner.java
@@ -1,5 +1,6 @@
 package combat;
 
+import entities.PlayerCharacter;
 import pools.BulletPool;
 import javafx.scene.image.Image;
 import javafx.scene.paint.Color;
@@ -11,16 +12,18 @@ public class PatternSpawner {
         this.bulletPool = bulletPool;
     }
 
-
     public void fire(FirePattern pattern, double originX, double originY, Image sprite, Color fallbackColor) {
         if (bulletPool != null && pattern != null) {
             pattern.execute(bulletPool, originX, originY, sprite, fallbackColor);
         }
     }
 
+    public void spawnPincer(double x, double y, int bulletsPerSide, double speed, double spreadAngle, PlayerCharacter target, Image sprite, Color color) {
+        fire(new PincerPattern(bulletsPerSide, speed, spreadAngle, target), x, y, sprite, color);
+    }
 
-    public void spawnPincer(double x, double y, double offset, int bulletsPerSide, double speed, double inwardAngleRad, Image sprite, Color color) {
-        fire(new PincerPattern((int) (offset * 2), bulletsPerSide, speed, inwardAngleRad), x, y, sprite, color);
+    public void spawnClaw(double x, double y, int bulletCount, double speed, double arcAngle, PlayerCharacter target, Image sprite, Color color) {
+        fire(new ClawPattern(bulletCount, speed, arcAngle, target), x, y, sprite, color);
     }
 
     public BulletPool getBulletPool() { return bulletPool; }
